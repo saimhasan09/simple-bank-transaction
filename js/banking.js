422f6040cb5497a26487905a7c1001f40e20b55e
saimhasan09 <saimhasannahid@gmail.com>
2021-08-16 10:45:42 +0600

    second

--- js/banking.js
+++ js/banking.js
@@ -25,7 +25,7 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     depositInput.value = '';
 
 
-    // ------------------------------  balance part      --------------------
+    // ------------------------------ deposit balance part      --------------------
 
     // update account balance
     const balanceTotal = document.getElementById('balance-total');
@@ -36,6 +36,50 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     //Balance calculation for deposit
     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+})
+
+// -----------------------------------        withdraw part   ------------------------
+
+// handle withdraw with event handler
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    
+    // get the amount
+    const withdrawInput = document.getElementById('withdraw-input');
+
+    //convert string to float
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    // console.log(newWidthdrawAmount);
+
+    // update withdraw
+    const withdrawTotal = document.getElementById('withdraw-total');
+
+    //convert string to float
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    // calculation
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // clear the deposit input field
+    withdrawInput.value = '';
+
+// ------------------------------ withdraw balance part      ------------------------
+
+// update account balance
+const balanceTotal = document.getElementById('balance-total');
+
+ //convert string to float
+ const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+
+    //Balance calculation for deposit
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
     balanceTotal.innerText = newBalanceTotal;
 
